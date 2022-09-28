b126b6875742e5fae4966d6dd7af5f89bc496761
dbdimitrov <d.boykoff@gmail.com>
2022-09-28 21:24:17 +0200

    reformat underscores

--- liana/utils/pre.py
+++ liana/utils/pre.py
@@ -23,39 +23,57 @@ def _check_if_covered(
             f"{subset_name} elements exceeded ({prop_missing:.2f}). "
         )
         raise ValueError(msg + f"{x_missing} missing from {superset_name}")
-    if verbose: f"{x_missing} missing from {superset_name}"
+    if verbose:
+        print(f"{x_missing} missing from {superset_name}")
 
 
 # Helper Function to check if the matrix is in the correct format
-def _check_mat(X, verbose=False):
+def _check_mat(x, verbose=False):
     # convert to sparse csr matrix
-    if not isinstance(X, csr_matrix):
+    if not isinstance(x, csr_matrix):
         if verbose:
             print("Converting mat to CSR format")
-        X = csr_matrix(X).copy()
+        x = csr_matrix(x).copy()
 
     # Check for empty features
-    msk_features = np.sum(X != 0, axis=0).A1 == 0
+    msk_features = np.sum(x != 0, axis=0).A1 == 0
     n_empty_features = np.sum(msk_features)
     if n_empty_features > 0:
         if verbose:
             print("{0} features of mat are empty, they will be removed.".format(
                 n_empty_features))
-        X = X[:, ~msk_features]
+        x = x[:, ~msk_features]
 
     # Check for empty samples
-    msk_samples = np.sum(X != 0, axis=1).A1 == 0
+    msk_samples = np.sum(x != 0, axis=1).A1 == 0
     n_empty_samples = np.sum(msk_samples)
     if n_empty_samples > 0:
         if verbose:
             print("{0} samples of mat are empty, they will be removed.".format(
                 n_empty_samples))
-        X = X[:, ~msk_features]
+        x = x[:, ~msk_features]
 
-    # Check for non finite values
-    if np.any(~np.isfinite(X.data)):
+    # Check for non-finite values
+    if np.any(~np.isfinite(x.data)):
         raise ValueError(
             """mat contains non finite values (nan or inf), please set them 
             to 0 or remove them.""")
 
-    return X
+    return x
+
+
+# Helper function to replace a substring in string and append to list
+def _append_replace(x, l):
+    l.append(x)
+    return x.replace('_', '')
+
+
+# format variable names
+def _format_vars(var_names, verbose=False):
+    changed = []
+    var_names = [_append_replace(x, changed) if ('_' in x) else x for x in
+                 var_names]
+    changed = ' ,'.join(changed)
+    if verbose & (len(changed) > 0):
+        print(f"Replace underscores (_) with blank in {changed}", )
+    return var_names
