26ab1ba2c06b616dda287c8da477bc70cec690c4
dbdimitrov <d.boykoff@gmail.com>
2022-10-18 12:55:05 +0200

    load resources from CSV + start resource retrieval tutorial

--- liana/resource/__init__.py
+++ liana/resource/__init__.py
@@ -1,2 +1,2 @@
-from .select_resource import explode_complexes
+from .utils import explode_complexes
 from .select_resource import select_resource
--- liana/resource/select_resource.py
+++ liana/resource/select_resource.py
@@ -2,30 +2,17 @@ from pandas import read_csv
 import pathlib
 
 
-# Placeholder function to read in resource
+# Read resource from CSV
 def select_resource(resource_name='consensus'):
-    resource_path = pathlib.Path(__file__).parent.joinpath(f"{resource_name}.csv")
-    print(resource_path)
-    resource = read_csv(resource_path, index_col=False)  ### TO BE CHANGED
+    resource_name = resource_name.lower()
+
+    resource_path = pathlib.Path(__file__).parent.joinpath("omni_resource.csv")
+    resource = read_csv(resource_path, index_col=False)
+    
+    resource = resource[resource['resource'] == resource_name]
 
     resource = resource[['source_genesymbol', 'target_genesymbol']]
     resource = resource.rename(columns={'source_genesymbol': 'ligand',
                                         'target_genesymbol': 'receptor'})
 
     return resource
-
-
-# Function to explode complexes (decomplexify Resource)
-def explode_complexes(resource):
-    resource['interaction'] = resource['ligand'] + '|' + resource['receptor']
-    resource = (resource.set_index('interaction')
-                .apply(lambda x: x.str.split('_'))
-                .explode(['receptor'])
-                .explode('ligand')
-                .reset_index()
-                )
-    resource[['ligand_complex', 'receptor_complex']] = resource[
-        'interaction'].str.split('|', expand=True)
-
-    return resource
-
