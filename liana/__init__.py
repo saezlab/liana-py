97eda881752a91cd51127f47248172d68006cf74
dbdimitrov <d.boykoff@gmail.com>
2022-11-10 14:56:21 +0100

    add ipykernel

--- liana/__init__.py
+++ liana/__init__.py
@@ -1,8 +1,9 @@
 __version__ = '0.0.1'  # noqa: F401
 __version_info__ = tuple([int(num) for num in __version__.split('.')])  # noqa: F401
 
+
+import liana.plotting as pl
+import steady as sc
 from .steady import liana_pipe, cellphonedb, natmi, singlecellsignalr,\
     connectome, logfc, rank_aggregate, show_methods
 from .resource.select_resource import select_resource, show_resources
-
-import liana.plotting as pl
