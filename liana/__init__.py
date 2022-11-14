c223ad3752620f11883deae3fe58c2d4f26d3b5c
dbdimitrov <d.boykoff@gmail.com>
2022-11-14 15:30:00 +0100

    rebuild docs

--- liana/__init__.py
+++ liana/__init__.py
@@ -3,7 +3,7 @@ __version_info__ = tuple([int(num) for num in __version__.split('.')])
 
 from liana import method as mt, plotting as pl, resource as rs
 
-# has to be done at the end, after everything has been imported
+# done after everything has been imported (adapted from scanpy)
 import sys
 
 sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['mt', 'pl', 'rs']})
