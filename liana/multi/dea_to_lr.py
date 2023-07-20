ecd244b2193d919cd27249d34a5bffe651c676fb
dbdimitrov <d.boykoff@gmail.com>
2023-07-20 17:44:27 +0200

    refactor dea + plots

--- liana/multi/dea_to_lr.py
+++ liana/multi/dea_to_lr.py
@@ -4,9 +4,8 @@ from itertools import product
 
 from liana.method._pipe_utils import prep_check_adata, assert_covered, filter_resource, \
     filter_reassemble_complexes
-from liana.resource import select_resource, explode_complexes
+from liana.resource import _handle_resource, explode_complexes
 from liana.method._liana_pipe import _get_props, _join_stats
-
 
 def dea_to_lr(adata,
               dea_df,
@@ -14,6 +13,7 @@ def dea_to_lr(adata,
               stat_keys,
               resource_name = 'consensus',
               resource = None,
+              interactions = None,
               layer = None, 
               use_raw = True,
               expr_prop = 0.1,
@@ -25,23 +25,71 @@ def dea_to_lr(adata,
               lr_sep="^",
               verbose = False,
               ):
+    """
+    Convert DEA results to ligand-receptor pairs.
+    
+    Parameters
+    ----------
+    adata : AnnData object
+        Anndata Object
+    dea_df : pd.DataFrame
+        DEA results. Index must match adata.var_names
+    groupby : str
+        Column in adata.obs and dea_df to groupby
+    stat_keys : list
+        List of statistics to use for lr pairs
+    resource_name : str, optional
+        Name of resource to use. Default is 'consensus'
+    resource : pd.DataFrame, optional
+        Resource to use. If None, will use resource_name
+    interactions : pd.DataFrame, optional
+        Interactions to use. If None, will use resource
+    layer : str, optional
+        Layer to use. If None, will use adata.raw.X
+    use_raw : bool, optional
+        Whether to use adata.raw.X. Default is True
+    expr_prop : float, optional
+        Minimum cells expressing a gene for it to be considered expressed. Default is 0.1
+    min_cells : int, optional
+        Minimum cells per group to be considered. Default is 10
+    complex_col : str, optional
+        Column in dea_df to use for complex expression. Default is None.
+        If None, will use mean expression ('expr') calculated per group in `groupby`.
+    return_all_lrs : bool, optional
+        Whether to return all ligand-receptor pairs. Default is False.
+        If False, will only return ligand-receptor pairs, the genes for which
+        have matching statistics in dea_df (for each group in `groupby`).
+    source_labels : list, optional
+        List of labels to use as source. Default is None
+    target_labels : list, optional
+        List of labels to use as target. Default is None
+    lr_sep : str, optional
+        Separator to use between ligand and receptor. Default is '^'
+    verbose : bool, optional
+        Whether to print progress. Default is False
+    
+    Returns
+    -------
+    Returns a pd.DataFrame with joined ligand-receptor pairs and statistics.        
+
+    """
+    
     _key_cols = ['source', 'target', 'ligand_complex', 'receptor_complex']
         
     if (groupby not in adata.obs.columns) or (groupby not in dea_df.columns):
         raise ValueError(f'groupby must match a column in adata.obs and dea_df')
-    if resource is None:
-        if resource_name is not None:
-            resource = select_resource(resource_name)
-        else:
-            raise ValueError('Please provide a `resource` or a valid `resource_name`')
-    if any('_' in key for key in stat_keys):
-        raise ValueError('stat_keys must not contain "_"')
+    if not np.any(adata.var_names.isin(dea_df.index)):
+        raise ValueError(f'index of dea_df must match adata.var_names')
+    
+    resource = _handle_resource(interactions=interactions,
+                                resource=resource,
+                                resource_name=resource_name,
+                                verbose=verbose)
     
     stat_names = ['expr', 'props'] + stat_keys
     if complex_col is not None:
         if complex_col not in stat_names:
             raise ValueError(f'complex_col must be one of {stat_names}')
-        # set complex_col as first column
         stat_names = stat_names[stat_names.index(complex_col):]+stat_names[:stat_names.index(complex_col)]
     else:
         complex_col = 'expr'
@@ -56,10 +104,10 @@ def dea_to_lr(adata,
                              )
     
     # reduce dim of adata
-    adata =  adata[:, dea_df.index.unique()].copy()
+    adata =  adata[:, dea_df.index.unique()]
     
     # get label cats
-    labels = adata.obs.label.cat.categories
+    labels = adata.obs['label'].cat.categories
     
     dedict = {}
     
@@ -77,10 +125,14 @@ def dea_to_lr(adata,
         # merge DEA results to props & means
         df = dea_df[dea_df[groupby] == label].drop(groupby, axis=1). \
             reset_index().rename(columns={'index': "names"})
-            
-        stats = stats.merge(df, on='names')
-        dedict[label] = stats[['names', 'label', *stat_names]]
         
+        if not return_all_lrs:
+            stats = stats.merge(df, on='names')
+        else: 
+            stats = df.merge(stats, on='names', how='outer')
+            
+        dedict[label] = stats[['names', 'label', *stat_names]]
+        all_stats = pd.concat(dedict.values())
         
     # Create df /w cell identity pairs
     pairs = pd.DataFrame(list(product(labels, labels))). \
@@ -96,10 +148,10 @@ def dea_to_lr(adata,
     # Check overlap between resource and adata
     assert_covered(np.union1d(np.unique(resource["ligand"]),
                                 np.unique(resource["receptor"])),
-                    dea_df.index, verbose=verbose)
+                    all_stats['names'], verbose=verbose)
 
     # Filter Resource
-    resource = filter_resource(resource, dea_df.index)
+    resource = filter_resource(resource, all_stats['names'].unique())
     
     # Join Stats to LR
     lr_res = pd.concat(
@@ -111,17 +163,20 @@ def dea_to_lr(adata,
     complex_cols = list(product(['ligand', 'receptor'], [complex_col]))
     complex_cols = [f'{x}_{y}' for x, y in complex_cols]
     
-    # assign receptor and ligand absolutes, NOTE to refactor this
-    lr_res[['ligand_absolute', 'receptor_absolute']] = \
+    # assign receptor and ligand absolutes, NOTE deals with missing values
+    _placeholders = ['ligand_absolute', 'receptor_absolute']
+    lr_res[_placeholders] = \
         lr_res[complex_cols].apply(lambda x: x.abs())
+    if return_all_lrs:
+        lr_res[_placeholders] = lr_res[_placeholders].fillna(0)
     
     lr_res = filter_reassemble_complexes(lr_res=lr_res,
                                          _key_cols=_key_cols,
                                          expr_prop=expr_prop,
                                          return_all_lrs=return_all_lrs,
-                                         complex_cols=['ligand_absolute', 'receptor_absolute']
+                                         complex_cols=_placeholders
                                         )
-    lr_res = lr_res.drop(['prop_min', 'interaction', 'ligand_absolute', 'receptor_absolute'], axis=1)
+    lr_res = lr_res.drop(['prop_min', 'interaction', *_placeholders], axis=1)
     
     # summarise stats for each lr
     for key in stat_keys:
---
+++ liana/plotting/_common.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+
+def _check_size_colour(liana_res, colour=None, size=None):
+    if colour is None:
+        raise ValueError('`colour` must be provided!')
+    if size is None:
+        raise ValueError('`size` must be provided!')
+    if colour not in liana_res.columns:
+        raise ValueError(f'`colour` must be one of {liana_res.columns}')
+    if size not in liana_res.columns:
+        raise ValueError(f'`size` must be one of {liana_res.columns}')
+
+
+def _prep_liana_res(adata=None,
+                    liana_res=None,
+                    source_labels=None,
+                    target_labels=None,
+                    ligand_complex=None,
+                    receptor_complex=None,
+                    uns_key='liana_res'):
+    
+    if (liana_res is None) & (adata is None):
+        raise AttributeError(f'Ambiguous! One of `liana_res` or `adata.uns[{uns_key}]` should be provided.')
+    if adata is not None:
+        assert uns_key in adata.uns_keys()
+        liana_res = adata.uns[uns_key].copy()
+    if liana_res is not None:
+        liana_res = liana_res.copy()
+    if (liana_res is None) & (adata is None):
+        raise ValueError('`liana_res` or `adata` must be provided!')
+
+    # subset to only cell labels of interest
+    liana_res = _filter_labels(liana_res, labels=source_labels, label_type='source')
+    liana_res = _filter_labels(liana_res, labels=target_labels, label_type='target')
+    
+    liana_res['interaction'] = liana_res['ligand_complex'] + ' -> ' + liana_res['receptor_complex']
+    
+    if ligand_complex is not None:
+        liana_res = liana_res[np.isin(liana_res['ligand_complex'], ligand_complex)]
+    if receptor_complex is not None:
+        liana_res = liana_res[np.isin(liana_res['receptor_complex'], receptor_complex)]
+
+    return liana_res
+
+
+def _filter_labels(liana_res, labels, label_type):
+    if labels is not None:
+        if labels is str:
+            labels = [labels]
+        covered = np.isin(labels, liana_res[label_type])
+        if not covered.all():
+            not_covered = np.array(labels)[~covered]
+            raise ValueError(f"{not_covered} not found in `liana_res['{label_type}']`!")
+        msk = np.isin(liana_res[label_type], labels)
+        liana_res = liana_res[msk]
+        
+    return liana_res
+        
+
+def _aggregate_scores(res, what, how, entities):
+    return res.groupby(entities).agg(score=(what, how)).reset_index()
+
+
+def _inverse_scores(score):
+    return -np.log10(score + np.finfo(float).eps)
+
+        
+
+def _filter_by(liana_res, filterby, filter_lambda):
+    if filterby is not None:
+        msk = liana_res[filterby].apply(filter_lambda)
+        relevant_interactions = np.unique(liana_res[msk].interaction)
+        liana_res = liana_res[np.isin(liana_res.interaction, relevant_interactions)]
+    
+    return liana_res
+        
+        
+def _get_top_n(liana_res, top_n, orderby, orderby_ascending, orderby_absolute):
+    
+    if top_n is not None:
+        # get the top_n for each interaction
+        if orderby is None:
+            ValueError("Please specify the column to order the interactions.")
+        if orderby_ascending is None:
+            ValueError("Please specify if `orderby` is ascending or not.")
+        if orderby_ascending:
+            how = 'min'
+        else:
+            how = 'max'
+            
+        if orderby_absolute:
+            key = abs
+        else:
+            key = None    
+        
+        top_lrs = _aggregate_scores(liana_res,
+                                    what=orderby,
+                                    how=how,
+                                    entities=['interaction',
+                                              'ligand_complex',
+                                              'receptor_complex']
+                                    )
+        top_lrs = top_lrs.sort_values('score', ascending=orderby_ascending, key=key).head(top_n).interaction
+        
+        # Filter liana_res to the interactions in top_lrs
+        liana_res = liana_res[liana_res.interaction.isin(top_lrs)]
+        
+    return liana_res
