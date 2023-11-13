4b5f8f7f9b3e4d66184626bb6665332de9ace7d8
dbdimitrov <d.boykoff@gmail.com>
2023-11-13 08:01:10 +0100

    extend misty plots

--- liana/plotting/_misty_plots.py
+++ liana/plotting/_misty_plots.py
@@ -5,14 +5,16 @@ from liana._constants import Keys as K, DefaultValues as V
 from liana._docs import d
 
 @d.dedent
-def target_metrics(misty,
-                   stat,
+def target_metrics(misty = None,
+                   stat = None,
+                   target_metrics = None,
                    top_n = None,
                    ascending = False,
                    key = None,
                    filterby = None,
                    filter_lambda: callable = None,
-                   figure_size: tuple = (7,5),
+                   figure_size: tuple = (5, 5),
+                   aggregate_fun = None,
                    return_fig: bool = V.return_fig):
     """
     Plot target metrics.
@@ -22,6 +24,8 @@ def target_metrics(misty,
     %(misty)s
     stat : str
         Statistic to plot
+    target_metrics
+        A target_metrics DataFrame
     %(top_n)s
     ascending : bool
         Whether to sort in ascending order
@@ -30,6 +34,7 @@ def target_metrics(misty,
     %(filterby)s
     %(filter_lambda)s
     %(figure_size)s
+    %(aggregate_fun)s
     %(return_fig)s
 
     Returns
@@ -38,7 +43,14 @@ def target_metrics(misty,
     Returns a plotnine plot.
 
     """
-    target_metrics = misty.uns[K.target_metrics].copy()
+    if target_metrics is not None:
+        target_metrics = target_metrics.copy()
+    elif misty is not None:
+        target_metrics = misty.uns[K.target_metrics].copy()
+    else:
+        raise ValueError("Provide either a misty object or a target_metrics DataFrame.")
+    if stat is None:
+        raise ValueError("Provide a statistic to plot")
 
     if filterby is not None:
         msk = target_metrics[filterby].apply(filter_lambda)
@@ -47,30 +59,38 @@ def target_metrics(misty,
         target_metrics = target_metrics.sort_values(stat, ascending=ascending, key=key).head(top_n)
 
     # get order of target by decreasing intra.R2
-    targets = target_metrics.sort_values(by=stat, ascending=False)['target'].values
+    if aggregate_fun is not None:
+        targets = target_metrics.groupby(['target']).agg({stat: aggregate_fun}).reset_index()
+    else:
+        targets = target_metrics.copy()
+
+    targets = targets.sort_values(by=stat, ascending=False)['target']
     # targets as categorical variable
     target_metrics['target'] = pd.Categorical(target_metrics['target'],
                                               categories=targets,
                                               ordered=True)
 
     p = (p9.ggplot(target_metrics, p9.aes(x='target', y=stat)) +
-         p9.geom_point(size=3) +
+         (p9.geom_boxplot() if aggregate_fun is not None else p9.geom_point(size=3)) +
          p9.theme_bw() +
          p9.theme(axis_text_x=p9.element_text(rotation=90),
                   figure_size=figure_size) +
-        p9.labs(x='Target')
-        )
+         p9.labs(x='Target')
+         )
 
     if return_fig:
         return p
     p.draw()
 
 @d.dedent
-def contributions(misty,
+def contributions(misty = None,
+                  target_metrics = None,
                   top_n=None,
                   ascending=False,
                   key=None,
-                  figure_size: tuple = (7, 5),
+                  view_names: list=None,
+                  aggregate_fun: callable = None,
+                  figure_size: tuple = (5, 5),
                   return_fig: bool = V.return_fig):
     """
     Plot view contributions per target.
@@ -79,11 +99,16 @@ def contributions(misty,
     ----------
 
     %(misty)s
+    target_metrics
+        A target_metrics DataFrame
     %(top_n)s
     ascending : bool
         Whether to sort in ascending order
     key : callable
         Function to use to sort the dataframe
+    view_names
+        A list of view names to plot
+    %(aggregate_fun)s
     %(figure_size)s
     %(return_fig)s
 
@@ -92,14 +117,25 @@ def contributions(misty,
     A plotnine plot.
 
     """
-    target_metrics = misty.uns[K.target_metrics].copy()
+    if target_metrics is not None:
+        target_metrics = target_metrics.copy()
+    elif misty is not None:
+        target_metrics = misty.uns[K.target_metrics].copy()
+    else:
+        raise ValueError("Provide either a misty object or a target_metrics DataFrame.")
 
-    view_names = misty.view_names.copy()
-    if 'intra' not in target_metrics.columns:
-        view_names.remove('intra')
+    if view_names is None:
+        if misty is None:
+            raise ValueError("Provide a list of view names to plot.")
+        view_names = misty.view_names.copy()
+        if 'intra' not in target_metrics.columns:
+            view_names.remove('intra')
 
     target_metrics = target_metrics[['target', *view_names]]
     target_metrics = target_metrics.melt(id_vars='target', var_name='view', value_name='contribution')
+
+    if aggregate_fun is not None:
+        target_metrics = target_metrics.groupby(['target']).agg({'contribution': aggregate_fun}).reset_index()
 
     if top_n is not None:
         target_metrics = target_metrics.sort_values('contribution', ascending=ascending, key=key).head(top_n)
@@ -117,15 +153,18 @@ def contributions(misty,
         return p
     p.draw()
 
+
 @d.dedent
-def interactions(misty,
-                 view,
+def interactions(misty= None,
+                 interactions = None,
+                 view = None,
                  top_n = None,
                  ascending = False,
                  key = None,
                  filterby = None,
                  filter_lambda: callable = None,
-                 figure_size: tuple = (7,5),
+                 aggregate_fun: callable = None,
+                 figure_size: tuple = (5,5),
                  return_fig: bool = V.return_fig):
     """
     Plot interaction importances.
@@ -143,6 +182,7 @@ def interactions(misty,
         Key to use when sorting interactions
     %(filterby)s
     %(filter_lambda)s
+    %(aggregate_fun)s
     %(figure_size)s
     %(return_fig)s
 
@@ -151,10 +191,21 @@ def interactions(misty,
     A plotnine plot.
 
     """
-    interactions = misty.uns[K.interactions].copy()
+    if interactions is not None:
+        interactions = interactions.copy()
+    elif misty is not None:
+        interactions = misty.uns[K.interactions].copy()
+    else:
+        raise ValueError("Provide either a misty object or interactions.")
+    if view is None:
+        raise ValueError("Provide a ``view`` to plot!")
+
     interactions = interactions[interactions['view'] == view]
     grouped = interactions.groupby('predictor')['importances'].apply(lambda x: x.isna().all())
     interactions = interactions[~interactions['predictor'].isin(grouped[grouped].index)]
+
+    if aggregate_fun is not None:
+        interactions = interactions.groupby(['target', 'predictor']).agg({'importances': aggregate_fun}).reset_index()
 
     if filterby is not None:
         top_interactions = interactions[interactions[filterby].apply(filter_lambda)]
--- liana/tests/test_misty_plots.py
+++ liana/tests/test_misty_plots.py
@@ -1,5 +1,4 @@
-
-from liana.plotting import interactions, contributions, target_metrics
+import liana.plotting as pl
 from liana.testing import generate_toy_spatial
 from liana.testing import _sample_target_metrics, _sample_interactions
 
@@ -8,17 +7,35 @@ adata.uns['target_metrics'] = _sample_target_metrics()
 adata.uns['interactions'] = _sample_interactions()
 adata.view_names = ['intra', 'extra']
 
+# test aggregate
+target_metrics = adata.uns['target_metrics'].copy()
+target_metrics = target_metrics._append(target_metrics)
+target_metrics['group'] = ['a'] * 3 + ['b'] * 3
+
+interactions = adata.uns['interactions'].copy()
+interactions = interactions._append(interactions)
+interactions['group'] = ['a'] * 9 + ['b'] * 9
 
 def test_target_contributions_plot():
-    contributions(misty=adata)
-    contributions(misty=adata, top_n=1, return_fig=False)
+    pl.contributions(misty=adata)
+    pl.contributions(misty=adata, top_n=1, return_fig=False)
 
 
 def test_target_metrics_plot():
-    target_metrics(misty=adata, stat='gain_R2')
-    target_metrics(misty=adata, stat='gain_R2', top_n=1, return_fig=False)
-    target_metrics(misty=adata, stat='gain_R2', filterby='multi_R2', filter_lambda=lambda x: x > 0.5)
+    pl.target_metrics(misty=adata, stat='gain_R2')
+    pl.target_metrics(misty=adata, stat='gain_R2', top_n=1, return_fig=False)
+    pl.target_metrics(misty=adata, stat='gain_R2', filterby='multi_R2', filter_lambda=lambda x: x > 0.5)
 
 
-def test_misty_interactions_plot():
-    interactions(misty=adata, top_n=3, view='extra', key=abs, ascending=False)
+def test_interactions_plot():
+    pl.interactions(misty=adata, top_n=3, view='extra', key=abs, ascending=False)
+
+
+def test_target_metrics_aggregate():
+    pl.target_metrics(target_metrics=target_metrics, stat='gain_R2', aggregate_fun='mean')
+
+def test_contributions_aggregate():
+    pl.contributions(target_metrics=target_metrics, view_names=['intra', 'extra'], aggregate_fun='median')
+
+def test_interactions_aggregate():
+    pl.interactions(interactions=interactions, view='intra', aggregate_fun='sum')
